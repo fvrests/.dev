51a6888843f946d3781d37f44549d87370aa1772
fvrests <fvrests@icloud.com>
2021-01-05 12:36:34 -0600

    wip: blog page

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,3 +1,24 @@
 <template>
-  <PageLayout />
+  <PageLayout>
+    <BaseSection>
+      <article>
+        <h1>{{ page.title }}</h1>
+        <nuxt-content :document="page" />
+      </article>
+    </BaseSection>
+  </PageLayout>
 </template>
+
+<script>
+import PageLayout from '../components/page-layout'
+import BaseSection from '../components/base-section'
+export default {
+  async asyncData({ $content }) {
+    const page = await $content('hello').fetch()
+
+    return {
+      page,
+    }
+  },
+}
+</script>
