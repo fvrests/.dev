eef2320c3a373a76b6681dcab35f0c82dcd16339
fvrests <fvrests@icloud.com>
2021-01-10 11:29:58 -0600

    add border to nav link hover state

--- components/desktop-nav.vue
+++ components/desktop-nav.vue
@@ -4,7 +4,7 @@
       <li v-for="link in links" :key="link.name">
         <nuxt-link
           :to="link.url"
-          class="link p-2 select-none rounded-md hover:text-night hover:border-3 hover:border-night"
+          class="link p-1 select-none rounded-md hover:text-night border-3 border-transparent hover:border-night light:hover:border-coal"
           :class="`hover:bg-${link.themeColor}`"
           :active-class="`bg-${link.themeColor} text-night border-3 border-night light:border-coal`"
           >{{ link.name }}</nuxt-link
@@ -13,25 +13,6 @@
     </ul>
   </nav>
 </template>
-
-<style scoped>
-/* .link:hover,
-.nuxt-link-active {
-  color: var(--color-night);
-} */
-@media (prefers-color-scheme: light) {
-  /* .link {
-    border: 3px solid transparent;
-    margin: 0px -3px;
-  } */
-  /* 
-  .link:hover, */
-  .nuxt-link-exact-active {
-    @apply border-opacity-100;
-  }
-  /* border: 3px solid var(--color-coal); */
-}
-</style>
 
 <script>
 export default {
