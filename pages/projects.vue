d684d605d3f12956c29797df52745ce6d97dd551
fvrests <fvrests@icloud.com>
2021-01-10 19:38:49 -0600

    add hover styles & transitions to project cards

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,29 +1,35 @@
 <template>
   <PageLayout>
     <h2
-      class="mx-12 lg:mx-36 mb-6 text-6xl lg:text-8xl font-display text-white light:text-coal"
+      class="mx-12 lg:mx-36 mb-6 text-6xl lg:text-7xl font-display text-white light:text-coal"
     >
       projects
     </h2>
-    <div class="grid grid-cols-1 xl:gap-8 xl:grid-cols-2 xl:mx-36 text-coal">
+    <div class="grid grid-cols-1 lg:gap-8 lg:grid-cols-2 lg:mx-36 text-coal">
       <nuxt-link
         v-for="project in Projects"
         :key="project.title"
         :to="project.links[0].url"
-        class="flex flex-col mx-12 lg:mx-36 xl:mx-0 mb-12 xl:mb-0 rounded-xl max-h-screen-sm p-8 hover:border border-8 border-transparent hover:shadow-xl transition-all duration-300"
+        class="card flex flex-col mx-12 lg:mx-0 mb-12 lg:mb-0 rounded-xl max-h-screen-sm p-8 hover:shadow-xl transition duration-300"
         :class="`bg-${project.themeColor}`"
       >
-        <h3 class="text-3xl lg:text-4xl font-display mb-1 self-end">
+        <h3
+          class="card-text text-3xl lg:text-4xl font-display mb-1 transition-all transform scale-100 duration-300"
+        >
           {{ project.title }}
         </h3>
-        <div class="self-end font-bold">{{ project.type }}</div>
+        <div
+          class="card-text font-bold transition-all transform scale-100 duration-300"
+        >
+          {{ project.type }}
+        </div>
         <div
           v-if="project.image"
-          class="border-8 border-night light:border-coal rounded-xl w-full mx-auto mt-8"
+          class="overflow-hidden border-8 border-night light:border-coal rounded-xl w-full mx-auto mt-8"
         >
           <img
             :src="require(`~/assets/images/${project.image}`)"
-            class="object-cover"
+            class="object-cover w-full h-full transform scale-100 transition-all duration-300"
           />
         </div>
       </nuxt-link>
@@ -37,6 +43,7 @@ import BaseSection from '../components/base-section'
 import BaseHeading from '../components/base-heading'
 import LinkButton from '../components/link-button'
 import Projects from '../data/projects.js'
+import { ref } from '@nuxtjs/composition-api'
 export default {
   props: {
     image: { type: String, default: null },
@@ -54,3 +61,12 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.card:hover img {
+  @apply scale-105;
+}
+.card:hover .card-text {
+  @apply -translate-y-1;
+}
+</style>
