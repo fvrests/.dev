3dbfa724c9167b56021832fa55df30eb1b1fc80f
fvrests <fvrests@icloud.com>
2021-01-15 12:26:40 -0600

    dynamic projects pages

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -1,16 +1,32 @@
 <template>
   <PageLayout>
-    <h1>{{ this.slug }}</h1>
-    <p>Path: {{ $route.path }}</p>
-    <p>Title: {{ this.title }}</p>
-    <div
-      v-if="this.image"
-      class="overflow-hidden border-8 border-night light:border-coal rounded-xl w-full mx-auto mt-8"
-    >
-      <img
-        :src="require(`~/assets/images/${this.image}`)"
-        class="object-cover w-full h-full transform scale-100 transition-all duration-300"
-      />
+    <div class="mx-12 lg:mx-36 flex flex-col items-center">
+      <div>
+        <h2 class="font-display text-4xl sm:text-5xl mb-2 sm:mb-4">
+          {{ project.title }}
+        </h2>
+        <div
+          class="card-text font-bold transform transition-all duration-300 mb-8 sm:mb-12"
+        >
+          {{ project.content }}
+        </div>
+        <div
+          v-if="project.image"
+          class="overflow-hidden border-8 -mx-4 border-night light:border-coal rounded-xl w-full max-w-screen-md mb-8 sm:mb-12"
+        >
+          <img
+            :src="require(`~/assets/images/${project.image}`)"
+            class="object-cover w-full h-full transform scale-100 transition-all duration-300"
+          />
+        </div>
+        <div class="space-x-8" v-if="project.links">
+          <template v-for="link in project.links">
+            <LinkButton :url="link.url" :themeColor="project.themeColor">{{
+              link.name
+            }}</LinkButton>
+          </template>
+        </div>
+      </div>
     </div>
   </PageLayout>
 </template>
@@ -18,6 +34,7 @@
 import Projects from '../../data/projects'
 import onMounted from '@nuxtjs/composition-api'
 import PageLayout from '../../components/page-layout'
+import LinkButton from '../../components/link-button'
 
 export default {
   async asyncData({ params }) {
@@ -28,12 +45,15 @@ export default {
     if (filteredProject) {
       return {
         slug: slug,
-        title: filteredProject.title,
-        image: filteredProject.image,
+        project: { ...filteredProject },
       }
     } else {
       redirect('/projects')
     }
+  },
+  components: {
+    PageLayout,
+    LinkButton,
   },
 }
 </script>
