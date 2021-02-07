8649c5e5e4f54dd22e76a9a6e2e6c2aae48fa7b1
fvrests <fvrests@icloud.com>
2021-02-07 17:42:45 -0600

    fix - text link applying to other .links

--- components/text-link.vue
+++ components/text-link.vue
@@ -1,17 +1,17 @@
 <template>
-	<a v-if="isExternal" class="link" :href="url"><slot /></a>
-	<nuxt-link v-else class="link" :to="url"><slot /></nuxt-link>
+	<a v-if="isExternal" class="text-link" :href="url"><slot /></a>
+	<nuxt-link v-else class="text-link" :to="url"><slot /></nuxt-link>
 </template>
 
 <style>
-.link {
+.text-link {
 	@apply font-bold opacity-90 hover:opacity-100 dark:text-paper dark:hover:text-rose relative;
 }
-.link::before {
+.text-link::before {
 	content: '';
 	@apply absolute h-1 -bottom-1 w-full left-0 bg-lavender transition-all duration-300;
 }
-.link:hover::before {
+.text-link:hover::before {
 	@apply bg-rose dark:bg-lavender;
 }
 </style>
