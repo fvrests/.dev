533d8118afd85e31eb3e0498c943968ad8060d71
fvrests <fvrests@icloud.com>
2021-02-21 19:21:03 -0600

    remove page border

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,11 @@
 <template>
-	<div class="w-full border-12 border-border h-full min-h-screen overscroll">
+	<div class="w-full h-full min-h-screen">
 		<BaseNav :isMenuOpen="isMenuOpen" :toggleMenu="toggleMenu" />
 		<div class="my-24 lg:my-36" :class="isMenuOpen ? 'hidden' : ''">
 			<Nuxt />
 		</div>
 	</div>
 </template>
-
-<style>
-/* set overscroll color on safari only */
-/* todo: doesn't work */
-/* .overscroll::after,
-.overscroll::before {
-  left: -1rem;
-  right: -1rem;
-  position: absolute;
-  background: var(--border);
-  height: 20.5rem;
-}
-.overscroll::before {
-  top: -20rem;
-}
-.overscroll::after {
-  bottom: -20rem;
-} */
-</style>
 
 <script>
 import { ref } from '@nuxtjs/composition-api'
