6c168e62bf209d95b9d01bd6ac944117c12ef7b3
fvrests <fvrests@icloud.com>
2021-02-21 21:58:03 -0600

    add rose pine org to github script

--- scripts/github.js
+++ scripts/github.js
@@ -2,10 +2,19 @@ const fetch = require('node-fetch')
 const fs = require('fs')
 
 module.exports = async function () {
-	console.log('Fetching new github data...')
-
+	console.log('Fetching github data...')
 	// GitHub API: https://developer.github.com/v3/repos/#get
-	return fetch('https://api.github.com/users/fvrests/repos').then((res) =>
-		fs.writeFileSync('/data/github.js', JSON.stringify(res, null, 2))
+
+	fetch('https://api.github.com/users/fvrests/repos').then((res) =>
+		fs.writeFileSync(
+			'/data/github-fvrests.js',
+			JSON.stringify(res, null, 2)
+		)
+	)
+	fetch('https://api.github.com/orgs/rose-pine/repos').then((res) =>
+		fs.writeFileSync(
+			'/data/github-rose-pine.js',
+			JSON.stringify(res, null, 2)
+		)
 	)
 }
